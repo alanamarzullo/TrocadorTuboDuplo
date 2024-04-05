dc35ffc387a41e08127bf9dc8c0ff4cf2e3614c5
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-04-05 19:06:40 -0300

    Update Caso_2_RevB.py

--- Caso_2_RevB.py
+++ Caso_2_RevB.py
@@ -68,11 +68,15 @@ def run_simulation(L, r1, r2, n, m1, Cp1, rho1, m2, Cp2, rho2, T1i, T2i, T0, U, 
 
     # Criação e exibição da figura 1
     fig_ani1 = plt.figure(figsize=(8,6))
+    plt.xlabel('Comprimento (m)')  
+    plt.ylabel('Temperatura do fluido 1 ( (°C)')  
     ani1 = FuncAnimation(fig_ani1, update_plot1, frames=df_Temp1.shape[0], repeat=False)
     save1 = ani1.save('Variação da Temperatura - Fluido 1.gif', writer='pillow', fps=10)
     
     # Criação e exibição da figura 2
     fig_ani2 = plt.figure(figsize=(8,6))
+    plt.xlabel('Comprimento (m)')  
+    plt.ylabel('Temperatura do fluido 2 ( (°C)')  
     ani2 = FuncAnimation(fig_ani2, update_plot2, frames=df_Temp2.shape[0], repeat=False)
     save2 = ani2.save('Variação da Temperatura - Fluido 2.gif', writer='pillow', fps=10)
     
