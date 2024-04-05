e10999ec454ca22a7cbf803ccf9b3ca7a58610a8
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-04-05 20:57:09 -0300

    Update Caso_2_RevB.py

--- Caso_2_RevB.py
+++ Caso_2_RevB.py
@@ -70,11 +70,19 @@ def run_simulation(L, r1, r2, n, m1, Cp1, rho1, m2, Cp2, rho2, T1i, T2i, T0, U, 
     fig_ani1 = plt.figure(figsize=(8,6))  
     ani1 = FuncAnimation(fig_ani1, update_plot1, frames=df_Temp1.shape[0], repeat=False)
     save1 = ani1.save('Variação da Temperatura - Fluido 1.gif', writer='pillow', fps=10)
+
+    # Definindo os títulos dos eixos x e y para a figura 1
+    plt.xlabel('Comprimento (m)')  # Título do eixo x
+    plt.ylabel('Temperatura (°C)')  # Título do eixo y
     
     # Criação e exibição da figura 2
     fig_ani2 = plt.figure(figsize=(8,6))
     ani2 = FuncAnimation(fig_ani2, update_plot2, frames=df_Temp2.shape[0], repeat=False)
     save2 = ani2.save('Variação da Temperatura - Fluido 2.gif', writer='pillow', fps=10)
+
+    # Definindo os títulos dos eixos x e y para a figura 1
+    plt.xlabel('Comprimento (m)')  # Título do eixo x
+    plt.ylabel('Temperatura (°C)')  # Título do eixo y
     
     # Exibindo a simulação
     with st.expander("Visualização da Simulação em tempo real para o Fluido 1 (frio) (Clique aqui para ver)"):
