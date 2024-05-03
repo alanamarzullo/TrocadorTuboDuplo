30a3f0d73c5c22e28c2ea30bd8b62f25715231ee
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-05-03 15:13:38 -0300

    Update Caso_2_RevB.py

--- Caso_2_RevB.py
+++ Caso_2_RevB.py
@@ -11,7 +11,7 @@ st.title('Simulador TROCAL - Simulação de Transferência de Calor em um Trocad
 st.write('Este é um simulador de um trocador de calor tubo duplo operando em correntes paralelas. Ao rodar a simulação, você poderá visualizar o perfil de temperatura dos fluidos 1 (frio) e 2 (quente) conforme o tempo passa. Você também poderá visualizar o gráfico de variação das temperaturas dos fluidos 1 e 2 quando o trocador atinge o regime permanente.')
 st.write('Este simulador utiliza as seguintes equações de balanço de energia para os fluidos frio e quente:')
 st.image('Equacoes Caso 2.png', use_column_width=True)
-st.write('ATENÇÃO: Ao final desta página, você também encontrará um botão que roda a simulação com um exemplo pré-definido ("Rodar exemplo padrão"). Este exemplo leva em torno de 5 minutos para rodar. Caso queira usar seus próprios valores, use o botão "Rodar simulação" e recomenda-se utilizar um número de nós pelo menos igual ou maior que 10, para melhor visualização dos resultados.')
+st.write('ATENÇÃO: Ao final desta página, você também encontrará um botão que roda a simulação com um exemplo pré-definido ("Rodar exemplo padrão"). Este exemplo leva em torno de 5 minutos para rodar. Caso queira usar seus próprios valores, use o botão "Rodar simulação" e recomenda-se utilizar um número de nós pelo menos igual ou maior que 10, para melhor visualização dos resultados. Além disso, a temperatura de entrada do fluido frio deve ser menor que a do frio quente, caso cantrário, o modelo pode não funcionar corretamente.')
 st.write('Abaixo há uma figura exemplificando este trocador, de autoria própria.')
 
 # Carregar a imagem
@@ -81,18 +81,18 @@ def run_simulation(L, r1, r2, n, m1, Cp1, rho1, m2, Cp2, rho2, T1i, T2i, T0, U, 
     
     # Exibindo a simulação
     with st.expander("Visualização da Simulação em tempo real para o Fluido 1 (frio) (Clique aqui para ver)"):
-        st.write('Variação da temperatura do fluido 1 ao longo do tempo e do comprimento.')
+        st.write('Variação da temperatura do fluido frio ao longo do tempo e do comprimento.')
         st.write('Tempo representado acima do GIF, em segundos. Temperaturas em Kelvin representadas na escala variável do eixo y. Comprimento do trocador representado em metros no eixo x do GIF.')
         st.image('Variação da Temperatura - Fluido 1.gif')
     with st.expander("Visualização da Simulação em tempo real para o Fluido 2 (quente) (Clique aqui para ver)"):
-        st.write('Variação da temperatura do fluido 2 ao longo do tempo e do comprimento.')
+        st.write('Variação da temperatura do fluido quente ao longo do tempo e do comprimento.')
         st.write('Tempo representado acima do GIF, em segundos. Temperaturas em Kelvin representadas na escala variável do eixo y. Comprimento do trocador representado em metros no eixo x do GIF.')
         st.image('Variação da Temperatura - Fluido 2.gif')
         
     # Exibindo o gráfico de variação da temperatura ao longo do comprimento em regime permanente para ambos os fluidos
     plt.figure(fig_permanente)
-    plt.plot(x, df_Temp1.iloc[-1, :] , color='blue', label='Fluido 1')
-    plt.plot(x, df_Temp2.iloc[-1, :], color='red', label='Fluido 2')
+    plt.plot(x, df_Temp1.iloc[-1, :] , color='blue', label='Fluido frio')
+    plt.plot(x, df_Temp2.iloc[-1, :], color='red', label='Fluido quente')
     plt.xlabel('Comprimento (m)')
     plt.ylabel('Temperatura (K)')
     plt.legend()
