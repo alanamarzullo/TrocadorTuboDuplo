cd477b5ae501fa41428ec4af0162b68e8119a50f
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-05-11 09:58:21 -0300

    Update Caso_2_RevB.py

--- Caso_2_RevB.py
+++ Caso_2_RevB.py
@@ -126,4 +126,4 @@ dt = st.number_input('Passo de Tempo (s)', min_value=0.0)
 if st.button('Rodar Simulação'):
     run_simulation(L, r1, r2, n, m1, Cp1, rho1, m2, Cp2, rho2, T1i, T2i, T0, U, dx, t_final, dt)
 elif st.button('Rodar Exemplo Padrão'):
-    run_simulation(25, 0.1, 0.15, 100, 3, 4180, 995.6, 5, 4180, 995.6, 400, 800, 300, 340, 25 / 100, 350, 1)
+    run_simulation(25, 0.1, 0.15, 100, 3, 4180, 1000, 5, 4180, 1000, 400, 800, 300, 340, 25 / 100, 350, 1)
